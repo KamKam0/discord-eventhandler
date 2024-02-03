b645c979bb000017140c5b98aa8c369edd98071c
KamKam0 <86293161+KamKam0@users.noreply.github.com>
2024-02-03 01:15:00 +0100

    bug fix commande feedback

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,23 +1,15 @@
 const Discord = require("@kamkam1_0/discord.js")
+const TextInput = new Discord.TextInput()
+.setCustomID("feedback_answer_content")
+.setMaxLength(1500)
+.setMinLength(20)
+.setRequired(true)
+.setStyle("Long")
 
 module.exports = async (bot, interaction, Langue) => {
   if(interaction.isSlash) bot.handler.analyse(bot, interaction)
   else if(interaction.isButton){
     if(interaction.custom_id === "response_ticket_button"){
-      const TextInput = new Discord.TextInput()
-      .setCustomID("feedback_answer_content")
-      .setLabel(Langue["feedbackAnswerContent"])
-      .setMaxLength(1500)
-      .setMinLength(20)
-      .setPlaceHolder(Langue["feedbackAnswerPlaceHolder"])
-      .setRequired(true)
-      .setStyle("Long")
-
-      const Modal = new Discord.Modal()
-      .setCustomID("modal_feedback_answer")
-      .setTitle(Langue["feedbackAnswerTitle"])
-      .AddTextInputs(TextInput)
-
       let replacemente = new Discord.Embed()
       .setTitle(interaction.message.embeds[0].title)
       .setThumbnail(interaction.message.embeds[0].thumbnail.url)
@@ -27,21 +19,31 @@ module.exports = async (bot, interaction, Langue) => {
       let disbutton = interaction.message.components[0]
       disbutton.disabled = true
       
+      TextInput
+      .setLabel(Langue["feedbackAnswerContent"])
+      .setPlaceHolder(Langue["feedbackAnswerPlaceHolder"])
+
+      const Modal = new Discord.Modal()
+      .setCustomID(`modal_feedback_answer_${interaction.message.embeds[0].footer.text.split(': ')[1]}`)
+      .setTitle(Langue["feedbackAnswerTitle"])
+      .AddTextInputs(TextInput)
+      
       interaction.message.modify({embeds: [replacemente], components: [disbutton]})
 
       interaction.reply({modal: Modal}).catch(err => {})
     }
   }
   else if(interaction.isModal){
-    if(interaction.custom_id === "modal_feedback_answer"){
+    if(interaction.custom_id.startsWith('modal_feedback_answer')){
       let content = interaction.getComponent("feedback_answer_content").value
-
+      
       let embed = new Discord.Embed()
       .setTitle(`${interaction.user.username}#${interaction.user.discriminator} ${Langue["feedbackAnswered"]}`)
       .setDescription("```" + content + "```")
       .setColor('#36393F')
-      bot.users.get(interaction.user.id)?.send({embeds: [embed]})
-      .catch(err => {})
+      
+      bot.users.get(interaction.custom_id.split('answer_')[1])?.send({embeds: [embed]}).catch(err => {})
+
       let resem = new Discord.Embed()
       .setDescription(`<@${interaction.user.id}>, ${Langue["answerSent"]}`)
       .setColor('#36393F')
